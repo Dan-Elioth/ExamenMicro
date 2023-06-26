c9f5dde77b18e54683946ee1087f319c51ea3059
jhamil545 <jhamil.apaza@upeu.edu.pe>
2023-06-26 09:14:32 -0500

    nuevo

--- service-employees/src/main/java/com/example/ionicreact/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ionicreact.repository;
-
-import com.example.ionicreact.entities.Employee;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
-}
--- service-suppliers/src/main/java/com/example/ionicreact/repository/SupplierRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ionicreact.repository;
-
-import com.example.ionicreact.entities.Supplier;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SupplierRepository extends CrudRepository<Supplier, Long> {
-}
