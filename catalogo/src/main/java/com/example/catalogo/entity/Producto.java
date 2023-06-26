b4145cd94f27e3d2219e0164f11e057691bd2b4d
Dan-Elioth <elitocp@gmail.com>
2023-06-26 11:01:22 -0500

    Catalogo

---
+++ catalogo/src/main/java/com/example/catalogo/entity/Producto.java
@@ -0,0 +1,42 @@
+package com.example.catalogo.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+@Entity
+@Data
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nombre;
+    private String precio;
+    private String detalle;
+
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "categoria_id")
+    @JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+
+    private Categoria categoria;
+    @Column(name = "fecha_creacion")
+    private LocalDateTime fechaCreacion;
+
+    @Column(name = "fecha_actualizacion")
+    private LocalDateTime fechaActualizacion;
+
+    @PrePersist
+    @PreUpdate
+    public void prePersistAndUpdate() {
+        LocalDateTime now = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
+        if (fechaCreacion == null) {
+            fechaCreacion = now;
+        }
+        fechaActualizacion = now;
+    }
+}
